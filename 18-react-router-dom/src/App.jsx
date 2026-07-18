7879d3e826220a97daa3df8a1311d108c76e1af0
Shahnaz Aktar Mazumder <shahnaj2507@gmail.com>
2026-07-18 21:03:23 +0530

    React router dom

---
+++ 18-react-router-dom/src/App.jsx
@@ -0,0 +1,27 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+
+function App() {
+  return (
+    <BrowserRouter>
+
+      <nav style={{ display: "flex", gap: "20px", marginBottom: "20px" }}>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+
+    </BrowserRouter>
+  );
+}
+
+export default App;
